b3e513b0e2aba1d5d6bc853f5691bfde0f0e32dc
user <dev@antalogic.com>
2020-08-06 12:49:46 +0300

    First commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="header">
+        <div className="header-container">
+            <h1>Test</h1>
+        </div>
+    </div>
+)
+
+export default Header;
