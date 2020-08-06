93b1da550f86153606dfe5c8ce8e0232f3a745d0
user <dev@antalogic.com>
2020-08-06 17:57:13 +0300

    Updates

---
+++ precache-manifest.2b83290771da162c76e989f3601004be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36be3ea34626c443fc43519ed31632a7",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "489f68801569467e2773",
+    "url": "/test-app/static/css/main.69b876f2.chunk.css"
+  },
+  {
+    "revision": "99847dcb80b4b919a6dc",
+    "url": "/test-app/static/js/2.4a2beeae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-app/static/js/2.4a2beeae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489f68801569467e2773",
+    "url": "/test-app/static/js/main.d4dd0843.chunk.js"
+  },
+  {
+    "revision": "010976abd684515c15d4",
+    "url": "/test-app/static/js/runtime-main.107bf183.js"
+  }
+]);
